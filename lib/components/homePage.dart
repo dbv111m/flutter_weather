0360a30a41a3744b7643e0f26222c4d86a7cc965
Dan <dbv111m@gmail.com>
2021-03-15 06:25:01 +0300

    tmp

---
+++ lib/components/homePage.dart
@@ -0,0 +1,14 @@
+import 'dart:convert';
+import 'package:geolocator/geolocator.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final Geolocator _geo = Geolocator()..forceAndroidLocationManager;
+@override 
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,25 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(),
+    MyApp(),
   );
 }
+
+
+class MyApp extends StatelessWidget {
+@override 
+Widget build(BuildContext context)
+{
+  return MaterialApp(
+    title: "Weather",
+    home: (
+      
+    )
+  ) 
+
+}
+
+
+}
+
+
