f4e81c01c3558daaa51807a1fd2e43f4f72a3418
Dan <dbv111m@gmail.com>
2021-03-16 02:17:48 +0300

    test 1 ok

--- lib/components/homePage.dart
+++ lib/components/homePage.dart
@@ -9,7 +9,53 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final Geolocator _geo = Geolocator()..forceAndroidLocationManager;
-@override 
-Widget build(BuildContext context)
+  Position _position;
+  String _city;
 
+  @override
+  void initState() {
+    _city = "test";
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          _city,
+        ),
+      ),
+    );
+  }
+
+  _getCurrent() {
+    _geo.getCurrentPosition(desiredAccuracy: LocationAccuracy.best).then(
+      (Position position) {
+        setState(() {
+          _position = position;
+          debugPrint(position.toString());
+        });
+
+        getCityAndWeather();
+      },
+    );
+  }
+
+  getCityAndWeather() async {
+    try {
+      List<Placemark> p = await _geo.placemarkFromCoordinates(
+        _position.latitude,
+        _position.longitude,
+      );
+      Placemark place = p[0];
+      setState(
+        () {
+          _city = place.locality;
+        },
+      );
+    } catch (e) {
+      print(e);
+    }
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_weather/components/homePage.dart';
 
 void main() {
   runApp(
@@ -6,21 +7,12 @@ void main() {
   );
 }
 
-
 class MyApp extends StatelessWidget {
-@override 
-Widget build(BuildContext context)
-{
-  return MaterialApp(
-    title: "Weather",
-    home: (
-      
-    )
-  ) 
-
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Weather",
+      home: HomePage(),
+    );
+  }
 }
-
-
-}
-
-
