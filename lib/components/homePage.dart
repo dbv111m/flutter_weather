b5155f6e9b209fa1a54e7cb9510c7bfb84aead1f
Dan <dbv111m@gmail.com>
2021-03-16 05:43:26 +0300

    Final

--- lib/components/homePage.dart
+++ lib/components/homePage.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
 import 'package:flutter_weather/components/searchForm.dart';
+import 'package:flutter_weather/components/weatherCard.dart';
+import 'package:flutter_weather/helpers/weather.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:flutter/material.dart';
 
@@ -12,12 +14,36 @@ class _HomePageState extends State<HomePage> {
   final Geolocator _geo = Geolocator()..forceAndroidLocationManager;
   Position _position;
   String _city;
+  int _temp;
+  String _icon;
+  String _desc;
+  WeatherFetch _weatherFetch = new WeatherFetch();
 
   @override
   void initState() {
-    _city = "test";
+    _city = "blanc";
+    _temp = 0;
+    _icon = "04n";
     super.initState();
     _getCurrent();
+  }
+
+  /* Render data */
+  void updateData(weatherData) {
+    setState(() {
+      if (weatherData != null) {
+        debugPrint(jsonEncode(weatherData));
+        //{"temp":10.49,"feels_like":5.54,"temp_min":10,"temp_max":11,"pressure":1009,"humidity":61}
+        _temp = weatherData['main']['temp'].toInt();
+        _icon = weatherData['weather'][0]['icon'];
+        _desc = weatherData['main']['feels_like'].toString();
+        // _color = _getBackgroudColor(_temp);
+      } else {
+        _temp = 0;
+        _city = "In the middle of nowhere";
+        _icon = "04n";
+      }
+    });
   }
 
   @override
@@ -36,6 +62,11 @@ class _HomePageState extends State<HomePage> {
                   fontSize: 40,
                   fontWeight: FontWeight.bold,
                 ),
+              ),
+              WeatherCard(
+                title: _desc,
+                temperature: _temp,
+                iconCode: _icon,
               )
             ],
           ),
@@ -65,8 +96,22 @@ class _HomePageState extends State<HomePage> {
       );
       Placemark place = p[0];
       print(place);
-      print(jsonEncode(place));
+      print(
+        jsonEncode(
+          place,
+        ),
+      );
       print(place.locality);
+      var data = await _weatherFetch.getWeatherByCoord(
+        _position.latitude,
+        _position.longitude,
+      );
+      debugPrint(
+        jsonEncode(
+          data,
+        ),
+      );
+      updateData(data);
       setState(
         () {
           _city = place.locality;
@@ -79,6 +124,8 @@ class _HomePageState extends State<HomePage> {
 
   _changeCity(city) async {
     try {
+      var data = await _weatherFetch.getWeatherByName(city);
+      updateData(data);
       setState(() {
         _city = city;
       });
